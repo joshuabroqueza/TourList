18284cdffbb71e3933758a945d323c6fadf5d169
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2020-04-14 11:07:32 +0800

    Add files via upload

---
+++ src/tourData.js
@@ -0,0 +1,33 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Cagsawa",
+    img: "./img/Cagsawa.jpg",
+    name: "Cagsawa tour",
+    info:"The Cagsawa Ruins are the remnants of a 16th-century Franciscan church, the Cagsawa church. It was originally built in the town of Cagsawa in 1587 but was burned down and destroyed by Dutch pirates in 1636. "
+    },
+  {
+    id: 2,
+    city: "Daraga Church",
+    img: "./img/DaragaChurch.jpg",
+    name: "Daraga Church Tour",
+    info:
+      "Nuestra Señora de la Porteria Parish Church, commonly known as Daraga Church, is a Roman Catholic Church in the municipality of Daraga, Albay, Philippines under the jurisdiction of the Roman Catholic Diocese of Legazpi. The church was built by the Franciscans in 1772 under the patronage of the Our Lady of the Gate"
+  },
+  {
+    id: 3,
+    city: "Quintinday Hills",
+    img: "./img/QuitindayHills.jpg",
+    name: "Quintinday Hills tour",
+    info:" Albay’s version of Bohol Chocolate Hills located in Barangay Quitinday. Qutinday Green Hills is approximately 15 kilometers from the town center. You can reach the place via motorcycle, tri-mobile, and four wheeled vehicles via Cotmon-Solong-Calabidong and Baligang-Caguiba routes."
+
+  },
+  {
+    id: 4,
+    city: "Sumlang Lake",
+    img: "./img/SumlangLake.jpg",
+    name: "Sumlang Lake tour",
+    info:
+      "One of Albay’s newest attractions, guests can have a relaxing ride in this 9-hectare swamp in an on a bamboo raft jazzed up with exquisite abaca furniture. It is best done towards sunset where the rafts can be framed in a snapshot with the picturesque Mayon in the background."
+  }
+];
